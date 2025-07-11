787f0bd0652e065385e81470ce23cdf336c32919
unknown <juanigmw>
2025-07-11 00:35:50 -0300

    Se Agregan las Rutas del Proyecto

--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -1,9 +1,30 @@
+import AccountPage from "../pages/AccountPage";
+import UsersPage from "../pages/admin/UsersPage";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import NotFoundPage from "../pages/NotFoundPage";
+import ProjectPage from "../pages/ProjectPage";
+import ProjectsPage from "../pages/ProjectsPage";
+import RegisterPage from "../pages/RegisterPage";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 export default function AppRouter()
 {
     return(
-           <div>
-                 <h1>AppRouter</h1>
-           </div>
+      
+        <Router>
+            <Routes>
+                <Route  path="/" element={<HomePage />}/>
+                <Route  path="/login" element={<LoginPage />}/>
+                 <Route path="/register" element={<RegisterPage />}/>
+                 <Route path="/account" element={<AccountPage />}/>
+                 <Route path="/projects" element={<ProjectsPage />}/>
+                 <Route path="/project/:projectId" element={<ProjectPage />}/>
+                 <Route path="/admin/users" element={<UsersPage />}/>
+                   <Route path="*" element={<NotFoundPage />}/>
+            </Routes>
+             
+        </Router>
+      
     )
 
 }
